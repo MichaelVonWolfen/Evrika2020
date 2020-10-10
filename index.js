653eb476b56c584f2c1ebacb6e05e22757cc054f
MichaelVonWolfen <mihai.stoica98@gmail.com>
2020-10-10 11:05:06 +0300

    Receive question from DB

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const http = require('http').createServer(app);
 
 const io = require("socket.io")(http);
 
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Salamandra#1',
+    database: 'evryka_dev'
+});
 
 http.listen(4000, () =>{
     console.log("Listening on port 4000");
@@ -22,5 +29,39 @@ io.on('connection', (socket) => {
     socket.on('message', (msg)=>{
         console.log(msg);
     });
+    socket.on('question', (msg)=>{
+        console.log(msg);
+        // get_question();
+        connection.query(
+            'select * from  questions\n' +
+                'where times_played like (select min(times_played) from questions)\n' +
+                'order by RAND()\n' +
+                'limit 1;',
+            function(err, results, fields) {
+                io.emit('rasp', err);
+                io.emit('rasp', results);
+                io.emit('rasp', fields);
+            }
+          );
+    });
 
 });
+
+function get_question(){
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Salamandra#1',
+        database: 'evryka_dev'
+    });
+    connection.query(
+        'SELECT * FROM `questions`',
+        function(err, results, fields) {
+            socket.br
+            console.log("error:\n" + err);
+            console.log("results:\n" + results); // results contains rows returned by server
+            console.log("fields:\n" + fields); // fields contains extra meta data about results, if available
+        }
+
+      );
+}
--- public/chat.js
+++ public/chat.js
@@ -1,11 +1,19 @@
 const socket = io();
+
+
+$('#question').click(function(){
+    socket.emit('question','type');
+});
 $('form').submit(function(e){
     try{
         e.preventDefault();
-        socket.emit('message', $('#m').val());
+        //socket.emit('message', $('#m').val());
          $('#m').val('');
         return false;
     }catch(err){
         console.error(err);
     }
+});
+socket.on('rasp',function(msg){
+    console.log(msg);
 });
