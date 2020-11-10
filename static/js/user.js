e0b46c6ccc9f92e3d04464434b033e8f24fb4f4f
Radu1999 <rchivereanu@gmail.com>
2020-11-10 22:46:32 +0200

    user on new time

--- static/js/user.js
+++ static/js/user.js
@@ -1,70 +1,86 @@
 jQuery(function() {
 	let response_sent  = 0;
-	let timp_pornire = new Date().getTime();
 
 	//1 cand userul a trimis raspuns ( daca nu apasa send in momentul terminarii timpului se va trimite automat ce e selectat)
+	disable_buttons() 
 
-	var procent=100;
+	const socket = io('/' + $('#namespace').val());
+	var idQuestion;
+	let curr_time = -1;
+	
+	socket.on('counter', function(count){
+		if(response_sent == 0) {
+			$('#seconds').text(count);
+		}
+		curr_time = count;
+	});
+
+	var procent = 100;
+
 	$('.progress-done').on("click", function() {
 		//console.warn(procent);
 		this.setAttribute('data-done', procent);
 		this.style.width = this.getAttribute('data-done') + '%';
 		this.style.opacity = 1;
-    });
-
-	let show_time = 14;
-	let y= setInterval(function() {
-		$('#seconds').text(`${show_time}`)
-		show_time--;
-		if(show_time == -1) {
-			clearInterval(y);
-		} else if(response_sent == 1) {
-			clearInterval(y);
-		}
- 	}, 1000);
-	var x = setInterval(function() {
-
-		var aux = new Date().getTime() - timp_pornire;
-
-        procent=(15000-aux)/150;
-		$('.progress-done').trigger('click');
-
-		if(response_sent == 1) {
-			procent = 0;
-			$('.progress-done').trigger('click');
-			clearInterval(x);
-		}
-
-		if ( aux > 15000) {
-
-			if ( response_sent == 0 ) {
-				pressed = $('.selected');
-				answer = $(pressed).attr("value");
-				name = $(pressed).attr("name");
-				send(answer, name);
-			}
-			clearInterval(x);
-		}
-	}, 50);
-
-	const socket = io('/' + $('#namespace').val());
-	var idQuestion;
-	socket.on('counter', function(count){
-		$('#timer').text(count);
-		console.log(count)
 	});
+
 	socket.on('answers', (msg) =>{
+
+		response_sent = 0;
+
 		$('#question').text(msg.question)
 		for(let i = 0; i<4; i++){
 			$(`#button${i}`).attr('name',msg.answers[i].name);
 			$(`#button${i}`).text(msg.answers[i].text);
 		}
-		enable_buttons()
+
+		enable_buttons();
+
+		refresh_selected();
+
+		procent=100;
+
+		$('.progress-done').trigger('click');
+
+		let aux = 15500;
+
+		var x = setInterval(function() {
+
+			aux -= 50;
+
+			if(aux - curr_time * 1000 > 500 && curr_time != -1) {
+				aux = curr_time * 1000;
+			}
+
+			procent = aux / 155;
+
+			$('.progress-done').trigger('click');
+
+			if(response_sent == 1) {
+				procent = 0;
+				$('.progress-done').trigger('click');
+				clearInterval(x);
+			}
+
+			if ( curr_time <= 0.01 && curr_time != -1) {
+
+				if ( response_sent == 0 ) {
+					pressed = $('.selected');
+					answer = $(pressed).attr("value");
+					name = $(pressed).attr("name");
+					send(answer, name);
+				}
+				clearInterval(x);
+			}
+		}, 50);
+
 		let king = $('#pid').attr('value')
+		curr_time = -1;
 	})
 
 	$('#button0,#button1,#button2,#button3').on("click", function(){
-		refresh_selected(this);
+		refresh_selected();
+		$(this).addClass('selected');
 	});
 
 	$('#submit').on("click", function(){
@@ -73,27 +89,25 @@ jQuery(function() {
 		name = $(pressed).attr("name");
 		send(answer, name);
 	});
-
-	function refresh_selected(button) {
-		pressed = $('.selected');
-		$(pressed).removeClass('selected');
-		$(button).addClass('selected');
+	
+	function refresh_selected() {
+		$('.selected').removeClass('selected');
 	}
-
+	
 	function send(answer, name) {
 		
 		disable_buttons()
-
+	
 		socket.emit('raspuns',{
 			personID: $('#pid').attr('value'),
 			answerID: name,
 			timerValue: $('#seconds').text()
 		})
-		response_sent = 1;
 		//for checking
+		response_sent = 1;
 		confirm(`Ai submis:${answer}\n`);
 	}
-
+	
 	function disable_buttons() {
 		for(i = 0; i < 4; i++) {
 			$(`#button${i}`).addClass('disabled');
