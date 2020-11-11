bb11f0f1a206acf4bb5c9679ff04bf61b00d5f2d
Radu1999 <rchivereanu@gmail.com>
2020-11-11 22:00:49 +0200

    better user page

--- static/js/user.js
+++ static/js/user.js
@@ -1,5 +1,5 @@
 jQuery(function() {
-	let response_sent  = 0;
+	let response_sent  = 1;
 
 	//1 cand userul a trimis raspuns ( daca nu apasa send in momentul terminarii timpului se va trimite automat ce e selectat)
 	disable_buttons() 
@@ -26,63 +26,94 @@ jQuery(function() {
 
 	socket.on('answers', (msg) =>{
 
-		response_sent = 0;
+		let king = $('#pid').attr('value')
 
-		$('#question').text(msg.question)
-		for(let i = 0; i<4; i++){
-			$(`#button${i}`).attr('name',msg.answers[i].name);
-			$(`#button${i}`).text(msg.answers[i].text);
-		}
+		console.log(king)
+		console.log(msg.kings[0] == king)
 
-		enable_buttons();
+		if(king == msg.kings[0] || king == msg.kings[1]) {
 
-		refresh_selected();
+			response_sent = 0;
 
-		procent=100;
-
-		$('.progress-done').trigger('click');
-
-		let aux = 15500;
-
-		var x = setInterval(function() {
-
-			aux -= 50;
-
-			if(aux - curr_time * 1000 > 500 && curr_time != -1) {
-				aux = curr_time * 1000;
+			$('#question').text(msg.question)
+			for(let i = 0; i<4; i++){
+				$(`#button${i}`).attr('name',msg.answers[i].name);
+				$(`#button${i}`).text(msg.answers[i].text);
 			}
 
-			procent = aux / 155;
+			enable_buttons();
+
+			refresh_buttons();
+
+			procent=100;
 
 			$('.progress-done').trigger('click');
 
-			if(response_sent == 1) {
-				procent = 0;
-				$('.progress-done').trigger('click');
-				clearInterval(x);
-			}
+			let aux = 15500;
 
-			if ( curr_time <= 0.01 && curr_time != -1) {
+			var x = setInterval(function() {
 
-				if ( response_sent == 0 ) {
-					pressed = $('.selected');
-					answer = $(pressed).attr("value");
-					name = $(pressed).attr("name");
-					send(answer, name);
+				aux -= 50;
+
+				if(aux - curr_time * 1000 > 500 && curr_time != -1) {
+					aux = curr_time * 1000;
 				}
-				clearInterval(x);
-			}
-		}, 50);
 
-		let king = $('#pid').attr('value')
-		curr_time = -1;
+				procent = aux / 155;
+
+				$('.progress-done').trigger('click');
+
+				if(response_sent == 1) {
+					procent = 0;
+					$('.progress-done').trigger('click');
+					clearInterval(x);
+				}
+
+				if ( curr_time <= 0.01 && curr_time != -1) {
+
+					if ( response_sent == 0 ) {
+						pressed = $('.selected');
+						answer = $(pressed).attr("value");
+						name = $(pressed).attr("name");
+						send(answer, name);
+					}
+					clearInterval(x);
+				}
+			}, 50);
+
+			curr_time = -1;
+		}
 	})
-	socket.on('allAnswers', (msg)=>{
+
+	socket.on('allAnswers', msg => {
 		console.log(msg)
+
+		for(let i = 0; i<4; i++){
+			if($(`#button${i}`).attr('name') == msg.answer_correctID) {
+				$(`#button${i}`).addClass('correctAnswer')
+			}
+		}
+
+		if(msg.IDanswerTeam1 == msg.IDanswerTeam2) {
+			console.log('here')
+			console.log(msg.IDanswerTeam1)
+			for(let i = 0; i<4; i++){
+				if($(`#button${i}`).attr('name') == msg.IDanswerTeam1) {
+					$(`#button${i}`).removeClass('selected')
+					$(`#button${i}`).addClass('bothSelected')
+				}
+			}
+		} else {
+			if($('.selected').attr('value') == msg.IDanswerTeam1) {
+				select_other_answer(msg.IDanswerTeam2)
+			} else {
+				select_other_answer(msg.IDanswerTeam1)
+			}
+		}
 	})
+
 	$('#button0,#button1,#button2,#button3').on("click", function(){
-		refresh_selected();
-		$(this).addClass('selected');
+		select_button(this);
 	});
 
 	$('#submit').on("click", function(){
@@ -92,8 +123,25 @@ jQuery(function() {
 		send(answer, name);
 	});
 	
-	function refresh_selected() {
+	function refresh_buttons() {
 		$('.selected').removeClass('selected');
+		$('.bothSelected').removeClass('bothSelected');
+		$('.correctAnswer').removeClass('correctAnswer');
+
+	}
+
+	function select_other_answer(answerID) {
+
+		if($(`#button${i}`).attr('name') == answerID) {
+			$(`#button${i}`).removeClass('selected')
+			$(`#button${i}`).addClass('team2Selected')
+		}
+
+	}
+
+	function select_button(button) {
+		$('.selected').removeClass('selected');
+		$(button).addClass('selected');
 	}
 	
 	function send(answer, name) {
